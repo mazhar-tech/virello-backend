1e26239cf5ff6231ac90b321c558ef2aad8aeca8
Muhammad Mazhar Iqbal <this.miqbal@gmail.com>
2025-09-16 22:30:47 +0500

    fix

--- test-email.js
+++ test-email.js
@@ -11,7 +11,7 @@ const testEmail = async () => {
   
   try {
     // Create transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
